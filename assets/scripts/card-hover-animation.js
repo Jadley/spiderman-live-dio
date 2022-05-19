d595f3d3a9acc65b1d7d1ecbf38f2e2638833a55
Jadley <jadleyd@gmail.com>
2022-05-19 11:10:35 -0300

    primeira fase concluida

--- assets/scripts/card-hover-animation.js
+++ assets/scripts/card-hover-animation.js
@@ -1,21 +1,21 @@
-function handleMouseEnter() {
+function handleMouseEnter(){
   this.classList.add('s-card--hovered');
   document.body.id = `${this.id}-hovered`;
 }
 
-function handleMouseLeave() {
-  this.classList.remove('s-card--hovered');
-  document.body.id = '';
-}
+function handleMouseLeave(){
+    this.classList.remove('s-card--hovered');
+    document.body.id = '';
+  }
 
-function addEventListenersToCards() {
+function addEventeListenersToCards(){
   const cardElements = document.getElementsByClassName('s-card');
-  
-  for (let index = 0; index < cardElements.length; index++) {
+ 
+  for (let index = 0; index < cardElements.length; index++){
     const card = cardElements[index];
     card.addEventListener('mouseenter', handleMouseEnter);
     card.addEventListener('mouseleave', handleMouseLeave);
   }
 }
 
-document.addEventListener("DOMContentLoaded", addEventListenersToCards, false);
+document.addEventListener("DOMContentLoaded", addEventeListenersToCards, false);
